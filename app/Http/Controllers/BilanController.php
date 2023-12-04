6a3e62553d30ef5bc1d453fe0d1d2832d2de7716
Tiorna225 <coolibalydeveloper2015@gmail.com>
2023-12-04 00:27:12 +0000

    first commit

--- app/Http/Controllers/BilanController.php
+++ app/Http/Controllers/BilanController.php
@@ -30,12 +30,22 @@ class BilanController extends Controller
 
     public function bilanchaud()
     {
-        $bilan = DB::select("select f.id_formation, f.theme_formation, sum(ea.note) as sum_note, count(ea.note) as count_note
-                    from formations f
-                        join evaluationschauds e on f.id_formation = e.id_formation
-                        join evaluationchaud_appreciations ea on e.id = ea.id_evaluationchaud
-                    group by f.id_formation, f.theme_formation
-                    order by f.id_formation");
+        $bilan = DB::select("SELECT
+                f.id_formation,
+                f.theme_formation,
+                SUM(ea.note) AS sum_note,
+                COUNT(ea.note) AS count_note
+            FROM
+                formations f
+                    JOIN
+                evaluationschauds e ON f.id_formation = e.id_formation
+                    JOIN
+                evaluationchaud_appreciations ea ON e.id = ea.id_evaluationchaud
+            GROUP BY
+                f.id_formation, f.theme_formation
+            ORDER BY
+                f.id_formation
+            ");
         return view("bilan_chaud", compact('bilan'));
     }
 }
